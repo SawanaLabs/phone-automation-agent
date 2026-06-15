d6e969ac48f21c21530022e14c068d3ff7a58c57
Sawana Huang <130306226+waitlistSawana@users.noreply.github.com>
2026-06-15 11:16:01 +0800

    chore: migrate linting to ultracite

--- apps/customer-android/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import { config as reactConfig } from "@workspace/eslint-config/react-internal"
-
-/** @type {import("eslint").Linter.Config} */
-export default [
-  ...reactConfig,
-  {
-    ignores: ["node_modules/**", ".expo/**", "dist/**", "web-build/**"],
-  },
-]
